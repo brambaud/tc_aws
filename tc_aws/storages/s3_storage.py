daa8ca53ccaeb9dcf3676e0832951f396b15c8a4
beram <beram.job@gmail.com>
2025-05-26 16:34:04 +0200

    fix: storage.get return None instead of raise the NoSuchKey error
    Tested Thumbor 7.7.4, when `STORAGE = 'tc_aws.storages.s3_storage'` it crashes due to a botocore.errorfactory.NoSuchKey error.
    
    Also become a little bit more consistent with result storage's get method which does the same.

--- tc_aws/storages/s3_storage.py
+++ tc_aws/storages/s3_storage.py
@@ -141,7 +141,7 @@ class Storage(AwsStorage, BaseStorage):
 
         try:
             file = await super(Storage, self).get(path)
-        except BotoCoreError:
+        except ClientError:
             return None
 
         async with file['Body'] as stream:
